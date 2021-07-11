a3a838738fa90e0cc15a47e52461cacc38050b77
Teodoro Garcia <tedtke514@gmail.com>
2021-07-10 19:06:09 -0500

    Initial commit

---
+++ lib/starships_cli.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "starships_cli/version"
+
+module StarshipsCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/starships_cli/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module StarshipsCli
+  VERSION = "0.1.0"
+end
